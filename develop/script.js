8ece4f4099aa828a4b301b1f963d419faad31d0a
Santiago Hincapie <santiagohr81@hotmail.com>
2020-09-28 19:26:23 -0400

    HTML layout  updated

---
+++ develop/script.js
@@ -0,0 +1,31 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+var characterAmountNumber = document.querySelector("#characterAmountNumber")
+
+
+
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+
+
+
+
+// Add event listener to generate button
+
+characterAmountNumber.addEventListener('input', characterAmountNumber)
+
+form.addEventListener('sumit', e => {
+  
+})
+
+generateBtn.addEventListener("click", writePassword);
+

8a9ee17e6cc74215073f9a57feef9d4f20db38ef
Santiago Hincapie <santiagohr81@hotmail.com>
2020-10-03 00:53:56 -0400

    input validation update

--- develop/script.js
+++ develop/script.js
@@ -7,11 +7,15 @@ var uppercase = document.querySelector("#uppercase");
 var numbers = document.querySelector("#numbers");
 var symbols = document.querySelector("#symbols");
 
-var lowerCaseCode = arrayLowToHigh(97, 122)
-console.log(lowerCaseCode)
+var lowerCaseCode = arrayLowToHigh(97, 122);
+console.log(lowerCaseCode);
 var upperCaseCode = arrayLowToHigh(65, 90);
 var numberCaseCode = arrayLowToHigh(48, 57);
-var symbolsCaseCode = arrayLowToHigh(32, 47).concat(arrayLowToHigh(58, 64)).concat(arrayLowToHigh(91, 96)).concat(arrayLowToHigh(58, 64)).concat(arrayLowToHigh(123, 126));
+var symbolsCaseCode = arrayLowToHigh(32, 47)
+  .concat(arrayLowToHigh(58, 64))
+  .concat(arrayLowToHigh(91, 96))
+  .concat(arrayLowToHigh(58, 64))
+  .concat(arrayLowToHigh(123, 126));
 
 // Write password to the #password input
 function writePassword() {
@@ -19,7 +23,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 function arrayLowToHigh(low, high) {
@@ -31,62 +34,66 @@ function arrayLowToHigh(low, high) {
   return array;
 }
 
-function generatePassLength(){
-  var passLength = document.getElementById("characterAmountNumber").value
+function generatePassLength() {
+  var passLength = document.getElementById("characterAmountNumber").value;
   document.getElementById("lengthDisplay").innerHTML = passLength;
-  console.log(passLength)
+  console.log(passLength);
   return passLength;
 }
 
-function generateCharArray(){
-  console.log("hi")
-  var charArray = []
+function generateCharArray() {
+  console.log("hi");
+  var charArray = [];
 
-  console.log(lowercase.checked)
-  if(lowercase.checked) {
-    charArray = charArray.concat(lowerCaseCode)
+  if (characterAmountNumber.value >= 8 && characterAmountNumber.value <= 128) {
+    if (
+      !lowercase.checked &&
+      !uppercase.checked &&
+      !numbers.checked &&
+      !symbols.checked
+    ) {
+      alert("Please select your criterion");
+    } else {
+      console.log(lowercase.checked);
+      if (lowercase.checked) {
+        charArray = charArray.concat(lowerCaseCode);
+      }
+      if (numbers.checked) {
+        charArray = charArray.concat(numberCaseCode);
+      }
+      console.log(charArray);
+      if (uppercase.checked) {
+        charArray = charArray.concat(upperCaseCode);
+      }
+      console.log(charArray);
+      if (symbols.checked) {
+        charArray = charArray.concat(symbolsCaseCode);
+      }
+    }
+  } else {
+    alert("Please select from 8 to 128 characters");
   }
-  if(numbers.checked){
-    charArray = charArray.concat(numberCaseCode)
-  }
-  console.log(charArray)
-  if(uppercase.checked){
-    charArray = charArray.concat(upperCaseCode)
-  }
-  console.log(charArray)
-  if(symbols.checked){
-    charArray = charArray.concat(symbolsCaseCode)
-  }
-  console.log(charArray)
-  if(symbols.checked === false && numbers.checked === false && lowercase.checked === false && uppercase.checked === false){
-    charArray = charArray.concat(lowerCaseCode)
-  }
-  console.log(charArray)
 
   return charArray;
-
 }
 
-function buildPassword(){
+function buildPassword() {
   var length = generatePassLength();
   console.log(length);
   var passArray = generateCharArray();
-  console.log(passArray)
-  var results = []
+  console.log(passArray);
+  var results = [];
 
-  for(var i = 0; i<length; i++) {
+  for (var i = 0; i < length; i++) {
     var index = Math.floor(Math.random() * passArray.length);
-    console.log(index)
+    console.log(index);
     var digit = passArray[index];
-    results.push(digit)
-    console.log(results)
-
+    results.push(digit);
+    console.log(results);
   }
-  return results.join("")
+  return results.join("");
 }
 
 // Add event listener to generate button
-
-
 
 generateBtn.addEventListener("click", writePassword);
